036d608b59662fb1dd3a9c3cdbbbf5b5a1ca4acb
xiti <xiti@pinduoduo.com>
2021-04-20 01:40:23 +0800

    更改状态栏颜色

---
+++ lib/page/home/home_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Feed'),
+          bottom: TabBar(
+            tabs: [
+              Tab(text: '知乎'),
+              Tab(text: '微博'),
+              Tab(text: 'GITHUB'),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Container(),
+            Container(),
+            Container(),
+          ],
+        ),
+      ),
+    );
+  }
+}
